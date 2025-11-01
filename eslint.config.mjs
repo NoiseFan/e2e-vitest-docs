a067f6f206d7eee13bef13ea8e0c977e1727d487
noise <noisefan@163.com>
2025-11-01 13:39:02 +0800

    chore: init repo

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import antfu from '@antfu/eslint-config'
+import playwright from 'eslint-plugin-playwright'
+
+
+export default antfu({
+  formatters: true,
+  typescript: true,
+  test: {
+    overrides: {
+      'test/padding-around-after-all-blocks': 'error',
+      'test/padding-around-after-each-blocks': 'error',
+      'test/padding-around-before-all-blocks': 'error',
+      'test/padding-around-before-each-blocks': 'error',
+      'test/padding-around-describe-blocks': 'error',
+      'test/padding-around-test-blocks': 'error',
+    },
+  }
+}, {
+    // @see https://github.com/mskelton/eslint-plugin-playwright
+    name: 'playwright',
+    ...playwright.configs['flat/recommended'],
+    files: ['**/*.spec.ts'],
+    rules: {
+      ...playwright.configs['flat/recommended'].rules,
+      'playwright/no-networkidle': 'off',
+      'playwright/expect-expect': 'off',
+      'playwright/no-conditional-in-test': 'off',
+    },
+  }
+)
